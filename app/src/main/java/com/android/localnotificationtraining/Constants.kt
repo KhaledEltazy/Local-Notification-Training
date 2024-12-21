346911179f096a3ab0b6e225f23719e516b27879
khaled <khmeltazy@gmail.com>
2024-12-21 22:53:41 +0200

    finishing simple local notification training app

---
+++ app/src/main/java/com/android/localnotificationtraining/Constants.kt
@@ -0,0 +1,7 @@
+package com.android.localnotificationtraining
+
+object Constants {
+    const val CHANNEL_ID = "1"
+    const val CHANNEL_NAME = "CHANNEL_NAME"
+
+}
--- app/src/main/java/com/android/localnotificationtraining/MainActivity.kt
+++ app/src/main/java/com/android/localnotificationtraining/MainActivity.kt
@@ -1,20 +1,63 @@
 package com.android.localnotificationtraining
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.localnotificationtraining.Constants.CHANNEL_ID
+import com.android.localnotificationtraining.Constants.CHANNEL_NAME
+import com.android.localnotificationtraining.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSendNotification.setOnClickListener {
+            counter++
+            binding.btnSendNotification.text = counter.toString()
+            if(counter %5 ==0){
+                startNotification()
+            }
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startNotification(){
+        val builder = NotificationCompat.Builder(this@MainActivity, CHANNEL_ID)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME,NotificationManager.IMPORTANCE_DEFAULT)
+            val manager : NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+
+            builder.apply {
+                setSmallIcon(R.drawable.small_icon)
+                setContentTitle("Local Notification")
+                setContentText("${counter}")
+            }
+
+        } else {
+            builder.apply {
+                setSmallIcon(R.drawable.small_icon)
+                setContentTitle("Local Notification")
+                setContentText("${counter}")
+                setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            }
+        }
+
+        val notificationMangerCompat = NotificationManagerCompat.from(this@MainActivity)
+        notificationMangerCompat.notify(1,builder.build())
     }
 }
